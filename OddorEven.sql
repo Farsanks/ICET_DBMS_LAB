45cbe55227dc3bba00547459394d42a735620c04
MUHAMMED FARSAN K S <121790355+Farsanks@users.noreply.github.com>
2025-06-11 22:13:12 +0530

    Create OddorEven.sql

---
+++ OddorEven.sql
@@ -0,0 +1,12 @@
+SET SERVEROUTPUT ON;
+
+DECLARE
+    num INTEGER := 7;  -- You can change this value for testing
+BEGIN
+    IF MOD(num, 2) = 0 THEN
+        DBMS_OUTPUT.PUT_LINE(num || ' is Even');
+    ELSE
+        DBMS_OUTPUT.PUT_LINE(num || ' is Odd');
+    END IF;
+END;
+/
